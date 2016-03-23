cb8ed24915e9a75d386a52b85c16ce72e90e40ec
Sophia Lindgren <sophia.lindgren@gmail.com>
2016-03-23 00:12:00 -0700

    Account controls on right-hand side now appear depending on cookies. Logout button now works.

---
+++ logout.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	    <link rel="stylesheet" href="style/validation.css">
+        <meta charset="utf-8" />
+        <title>Tea Time: Logging out...</title>
+    </head>
+    <body>
+    </body>
+</html>
+
+<?php
+
+if(isset($_COOKIE["username"])) {
+setcookie("username", "", time() - 3600);
+}
+if(isset($_COOKIE["userid"])) {
+setcookie("userid", "", time() - 3600);
+}
+    
+
+echo "<script type='text/javascript'>
+    window.location = 'index.html';
+    </script>";
+
+?>
